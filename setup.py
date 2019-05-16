9ca07f6c9ca50f973fd2c806b4bee2dc3399cc6c
semihgunel <gunelsemih@gmail.com>
2019-05-16 14:11:39 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,29 @@
 from setuptools import setup
 
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
         name='deepfly',
         packages=['deepfly'],
-        entry_points = {'console_scripts' : ['drosoph = deepfly.GUI.main:main']}
+        entry_points = {'console_scripts' : ['drosoph = deepfly.GUI.main:main']},
+        author="Semih Gunel",
+        author_email="semih.gunel@epfl.ch",
+        description="GUI and 3D pose estimation pipeline for tethered Drosophila.",
+        long_description=long_description,
+        url="https://github.com/NeLy-EPFL/anipose",
+        install_requires=[
+        'PyQt5',
+        'sklearn',
+        'numpy',
+        'scipy',
+        'scikit-video',
+        'skimage',
+        'matplotlib',
+        'torchvision',
+        ]
         )
+
+
+
