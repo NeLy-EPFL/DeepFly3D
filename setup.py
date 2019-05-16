2ac95c90cda64ef549cfad3950cc142a9469ac65
semihgunel <gunelsemih@gmail.com>
2019-05-16 14:22:36 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 from setuptools import setup
 
-
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
@@ -12,17 +11,17 @@ setup(
         author_email="semih.gunel@epfl.ch",
         description="GUI and 3D pose estimation pipeline for tethered Drosophila.",
         long_description=long_description,
+        long_description_content_type="text/markdown",
         url="https://github.com/NeLy-EPFL/anipose",
-        install_requires=[
-        'PyQt5',
-        'sklearn',
-        'numpy',
-        'scipy',
-        'scikit-video',
-        'skimage',
-        'matplotlib',
-        'torchvision',
-        ]
+        install_requires=['PyQt5',
+                          'sklearn',
+                          'numpy',
+                          'scipy',
+                          'scikit-video',
+                          'skimage',
+                          'matplotlib',
+                          'torchvision',
+                        ]
         )
 
 
