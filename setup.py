697040d0aafc675058d18a27cf1295adefe8f5d2
semihgunel <gunelsemih@gmail.com>
2021-12-20 18:59:22 +0100

    removing old notebooks; final setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import setuptools
 from setuptools import setup
 
 with open("README.md", "r") as fh:
@@ -5,17 +6,17 @@ with open("README.md", "r") as fh:
 
 setup(
     name="df3d",
-    version="0.52",
-    packages=["df3d"],
+    version="0.54",
+    description="GUI and 3D pose estimation pipeline for tethered Drosophila.",
+    author="Semih Gunel",
+    author_email="gunelsemih@gmail.com",
     entry_points={
         "console_scripts": ["df3d = df3d.gui:main", "df3d-cli = df3d.cli:main"]
     },
-    author="Semih Gunel",
-    author_email="gunelsemih@gmail.com",
-    description="GUI and 3D pose estimation pipeline for tethered Drosophila.",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
     long_description=long_description,
     long_description_content_type="text/markdown",
-    include_package_data=True,
     url="https://github.com/NeLy-EPFL/DeepFly3D",
     install_requires=[
         "PyQt5",
