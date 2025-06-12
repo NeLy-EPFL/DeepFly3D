a0135ae27b7106332f3b6595ce71883cf18f69bb
Jasper Phelps <jasper.s.phelps@gmail.com>
2025-06-12 23:57:15 +0200

    Add small one-liner script to show users how to run df3d on sample data

---
+++ tests/run_df3d_on_sample_data.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# You can run this to confirm that your DeepFly3D installation is working correctly.
+# If successful, you will get df3d results files in the directory `data/reference/df3d/`
+
+df3d-cli data/reference -v --order 0 1 2 3 4 5 6 --video-2d --video-3d
