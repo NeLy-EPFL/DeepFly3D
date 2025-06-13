dcea284f555e81ef54893a5e6cc1429352e552fb
Dominic D <dominicd7@hotmail.com>
2025-06-13 16:34:46 +0200

    Update tests for new output dir convention and fix change in test video data (#61)
    * Add a new tests for the default output dir and update `output_folder` parameter of Core
    Tests [#59](https://github.com/NeLy-EPFL/DeepFly3D/issues/59)
    
    * Run formatter on tests
    
    * Revert changes to video_pose3d.mp4 test data introduced in 3a1424a24664f3e1c5f7968ded39042f4cf02a0d
    
    * Update imports in test script
    Fixes warning that `df3d.video` was being overshadowed by a variable called `video`

--- tests/test_df3d.py
+++ tests/test_df3d.py
@@ -3,15 +3,17 @@ import os
 import pathlib
 import pickle
 import random
+import shlex
 import shutil
+import subprocess
 import unittest
 
 import cv2
 import numpy as np
 import torch
 
-from df3d import video
-from df3d.core import Core
+import df3d.core
+import df3d.video
 
 TEST_DATA_LOCATION = str(pathlib.Path(__file__).parent / "data")
 TEST_DATA_LOCATION_REFERENCE = f"{TEST_DATA_LOCATION}/reference"
@@ -21,7 +23,8 @@ TEST_DATA_LOCATION_RESULT_FILE_3D = f"{TEST_DATA_LOCATION_RESULT}/df3d_result_3d
 TEST_DATA_LOCATION_REFERENCE_VIDEO_2D = f"{TEST_DATA_LOCATION_RESULT}/video_pose2d.mp4"
 TEST_DATA_LOCATION_REFERENCE_VIDEO_3D = f"{TEST_DATA_LOCATION_RESULT}/video_pose3d.mp4"
 TEST_DATA_LOCATION_WORKING = f"{TEST_DATA_LOCATION}/working"
-TEST_DATA_LOCATION_WORKING_RESULT = f"{TEST_DATA_LOCATION_WORKING}/df3d"
+TEST_DATA_LOCATION_WORKING_RESULT = f"{TEST_DATA_LOCATION_WORKING}_df3d"
+
 
 def reset_rngs():
     # See: https://pytorch.org/docs/stable/notes/randomness.html
@@ -29,27 +32,35 @@ def reset_rngs():
     random.seed(0)
     np.random.seed(0)
 
+
 def clear_working_data():
     if os.path.exists(TEST_DATA_LOCATION_WORKING):
         shutil.rmtree(TEST_DATA_LOCATION_WORKING)
+    if os.path.exists(TEST_DATA_LOCATION_WORKING_RESULT):
+        shutil.rmtree(TEST_DATA_LOCATION_WORKING_RESULT)
+
 
 def load_videos():
     os.makedirs(TEST_DATA_LOCATION_WORKING, exist_ok=True)
     for video in glob.glob(f"{TEST_DATA_LOCATION_REFERENCE}/*.mp4"):
         shutil.copy(video, TEST_DATA_LOCATION_WORKING)
 
+
 def load_images():
     os.makedirs(TEST_DATA_LOCATION_WORKING, exist_ok=True)
     for image in glob.glob(f"{TEST_DATA_LOCATION_REFERENCE}/*.jpg"):
         shutil.copy(image, TEST_DATA_LOCATION_WORKING)
 
+
 def load_results_2d():
     os.makedirs(TEST_DATA_LOCATION_WORKING_RESULT, exist_ok=True)
     shutil.copy(TEST_DATA_LOCATION_RESULT_FILE_2D, get_results_save_path())
 
+
 def load_results_3d():
     os.makedirs(TEST_DATA_LOCATION_WORKING_RESULT, exist_ok=True)
     shutil.copy(TEST_DATA_LOCATION_RESULT_FILE_3D, get_results_save_path())
+
 
 def get_results_save_path():
     return os.path.join(
@@ -57,19 +68,24 @@ def get_results_save_path():
         "df3d_result_{}.pkl".format(TEST_DATA_LOCATION_WORKING.replace("/", "_")),
     )
 
+
 def get_results_2d():
     with open(TEST_DATA_LOCATION_RESULT_FILE_2D, "rb") as f:
         return pickle.load(f)
-    
+
+
 def get_results_3d():
     with open(TEST_DATA_LOCATION_RESULT_FILE_3D, "rb") as f:
         return pickle.load(f)
-    
+
+
 def get_video_2d_frames():
     return get_video_frames(TEST_DATA_LOCATION_REFERENCE_VIDEO_2D)
 
+
 def get_video_3d_frames():
     return get_video_frames(TEST_DATA_LOCATION_REFERENCE_VIDEO_3D)
+
 
 def get_video_frames(video_path: str):
     cap = cv2.VideoCapture(video_path)
@@ -94,39 +110,49 @@ class TestDeepFly3D(unittest.TestCase):
         In this case we need to convert the videos to images first."""
         load_videos()
 
-        core = Core(
+        core = df3d.core.Core(
             input_folder=TEST_DATA_LOCATION_WORKING,
-            output_folder="df3d",
+            output_folder=TEST_DATA_LOCATION_WORKING_RESULT,
             num_images_max=0,
             camera_ordering=[0, 1, 2, 3, 4, 5, 6],
         )
 
         self.assertEqual(core.num_images, 15, "Core didn't find all images in folder")
-        self.assertEqual(core.image_shape, [960,480], "Core didn't get the right image shape")
-        self.assertTrue(np.all(core.camera_ordering == np.array([0, 1, 2, 3, 4, 5, 6])), "Core didn't get correct camera ordering")
+        self.assertEqual(
+            core.image_shape, [960, 480], "Core didn't get the right image shape"
+        )
+        self.assertTrue(
+            np.all(core.camera_ordering == np.array([0, 1, 2, 3, 4, 5, 6])),
+            "Core didn't get correct camera ordering",
+        )
 
     def test_load_core_with_images(self):
         """Test that we can create the Core in a folder that already contains images"""
         load_images()
 
-        core = Core(
+        core = df3d.core.Core(
             input_folder=TEST_DATA_LOCATION_WORKING,
-            output_folder="df3d",
+            output_folder=TEST_DATA_LOCATION_WORKING_RESULT,
             num_images_max=0,
             camera_ordering=[0, 1, 2, 3, 4, 5, 6],
         )
 
         self.assertEqual(core.num_images, 15, "Core didn't find all images in folder")
-        self.assertEqual(core.image_shape, [960,480], "Core didn't get the right image shape")
-        self.assertTrue(np.all(core.camera_ordering == np.array([0, 1, 2, 3, 4, 5, 6])), "Core didn't get correct camera ordering")
+        self.assertEqual(
+            core.image_shape, [960, 480], "Core didn't get the right image shape"
+        )
+        self.assertTrue(
+            np.all(core.camera_ordering == np.array([0, 1, 2, 3, 4, 5, 6])),
+            "Core didn't get correct camera ordering",
+        )
 
     def test_pose_estimation(self):
         """Test that we can run pose estimation on images and get the right 2D points"""
         load_images()
 
-        core = Core(
+        core = df3d.core.Core(
             input_folder=TEST_DATA_LOCATION_WORKING,
-            output_folder="df3d",
+            output_folder=TEST_DATA_LOCATION_WORKING_RESULT,
             num_images_max=0,
             camera_ordering=[0, 1, 2, 3, 4, 5, 6],
         )
@@ -134,25 +160,47 @@ class TestDeepFly3D(unittest.TestCase):
 
         reference_results = get_results_2d()
 
-        assert core.points2d is not None, "2D pose estimation completely failed - no points are available"
-        np.testing.assert_allclose(core.points2d, reference_results["points2d"], err_msg="2D pose estimation points not correct.", atol=0.02)
-        np.testing.assert_allclose(core.conf, reference_results["heatmap_confidence"], err_msg="2D pose estimation confidence heatmaps not correct.", atol=0.002)
+        assert core.points2d is not None, (
+            "2D pose estimation completely failed - no points are available"
+        )
+        np.testing.assert_allclose(
+            core.points2d,
+            reference_results["points2d"],
+            err_msg="2D pose estimation points not correct.",
+            atol=0.02,
+        )
+        np.testing.assert_allclose(
+            core.conf,
+            reference_results["heatmap_confidence"],
+            err_msg="2D pose estimation confidence heatmaps not correct.",
+            atol=0.002,
+        )
 
         core.save()
 
         with open(core.save_path, "rb") as f:
             saved_pose_data = pickle.load(f)
 
-        np.testing.assert_allclose(saved_pose_data["points2d"], reference_results["points2d"], err_msg="2D pose estimation points not saved correctly.", atol=0.02)
-        np.testing.assert_allclose(saved_pose_data["heatmap_confidence"], reference_results["heatmap_confidence"], err_msg="2D pose estimation confidence heatmaps not saved correctly.", atol=0.002)
+        np.testing.assert_allclose(
+            saved_pose_data["points2d"],
+            reference_results["points2d"],
+            err_msg="2D pose estimation points not saved correctly.",
+            atol=0.02,
+        )
+        np.testing.assert_allclose(
+            saved_pose_data["heatmap_confidence"],
+            reference_results["heatmap_confidence"],
+            err_msg="2D pose estimation confidence heatmaps not saved correctly.",
+            atol=0.002,
+        )
 
     def test_calibration(self):
         """Test that we can run calibration to triangulate the 2D points into 3D points"""
         load_images()
         # FIX: can't load in 2d results from pose estimation and resume from there - CameraNetwork tries to load calib data which doesn't exist
-        core = Core(
+        core = df3d.core.Core(
             input_folder=TEST_DATA_LOCATION_WORKING,
-            output_folder="df3d",
+            output_folder=TEST_DATA_LOCATION_WORKING_RESULT,
             num_images_max=0,
             camera_ordering=[0, 1, 2, 3, 4, 5, 6],
         )
@@ -168,13 +216,28 @@ class TestDeepFly3D(unittest.TestCase):
 
         with open(core.save_path, "rb") as f:
             saved_pose_data = pickle.load(f)
-        
-        np.testing.assert_allclose(saved_pose_data["points3d_wo_procrustes"], reference_results["points3d_wo_procrustes"], err_msg="3D pose estimation points3d_wo_procrustes not correct.", atol=1e-5)
-        np.testing.assert_allclose(saved_pose_data["points3d"], reference_results["points3d"], err_msg="3D pose estimation points3d not correct.", atol=1e-5)
+
+        np.testing.assert_allclose(
+            saved_pose_data["points3d_wo_procrustes"],
+            reference_results["points3d_wo_procrustes"],
+            err_msg="3D pose estimation points3d_wo_procrustes not correct.",
+            atol=1e-5,
+        )
+        np.testing.assert_allclose(
+            saved_pose_data["points3d"],
+            reference_results["points3d"],
+            err_msg="3D pose estimation points3d not correct.",
+            atol=1e-5,
+        )
 
         def check_cameras_match(camera: int):
             for key in saved_pose_data[camera].keys():
-                np.testing.assert_allclose(saved_pose_data[camera][key], reference_results[camera][key], err_msg="3D pose estimation camera {camera} calibration property {key} not correct.", atol=1e-4)
+                np.testing.assert_allclose(
+                    saved_pose_data[camera][key],
+                    reference_results[camera][key],
+                    err_msg="3D pose estimation camera {camera} calibration property {key} not correct.",
+                    atol=1e-4,
+                )
 
         for camera_id in range(7):
             check_cameras_match(camera_id)
@@ -183,51 +246,120 @@ class TestDeepFly3D(unittest.TestCase):
         """Test that we can generate a video of the 2D pose estimation results"""
         load_images()
         load_results_3d()
-        core = Core(
+        core = df3d.core.Core(
             input_folder=TEST_DATA_LOCATION_WORKING,
-            output_folder="df3d",
+            output_folder=TEST_DATA_LOCATION_WORKING_RESULT,
             num_images_max=0,
             camera_ordering=[0, 1, 2, 3, 4, 5, 6],
         )
 
-        video.make_pose2d_video(
+        df3d.video.make_pose2d_video(
             core.plot_2d, core.num_images, core.input_folder, core.output_folder
         )
 
-        video_name = 'video_pose2d_' + core.input_folder.replace('/', '_') + '.mp4'
+        video_name = "video_pose2d_" + core.input_folder.replace("/", "_") + ".mp4"
         video_path = os.path.join(core.output_folder, video_name)
-        self.assertTrue(os.path.exists(video_path), f"Video of 2D poses wasn't successfully created - does not exist at {core.output_folder}/{video_name}")
+        self.assertTrue(
+            os.path.exists(video_path),
+            f"Video of 2D poses wasn't successfully created - does not exist at {core.output_folder}/{video_name}",
+        )
 
         reference_frames = get_video_2d_frames()
         test_frames = get_video_frames(video_path)
 
-        self.assertEqual(len(test_frames), len(reference_frames), "Number of frames in output video doesn't match what it should")
-        for frame, (test_frame, reference_frame) in enumerate(zip(test_frames, reference_frames)):
-            np.testing.assert_almost_equal(test_frame, reference_frame, err_msg=f"Frame {frame} of 2D video doesn't match what it should")
+        self.assertEqual(
+            len(test_frames),
+            len(reference_frames),
+            "Number of frames in output video doesn't match what it should",
+        )
+        for frame, (test_frame, reference_frame) in enumerate(
+            zip(test_frames, reference_frames)
+        ):
+            np.testing.assert_almost_equal(
+                test_frame,
+                reference_frame,
+                err_msg=f"Frame {frame} of 2D video doesn't match what it should",
+            )
 
     def test_video_3d(self):
         """Test that we can generate a video of the 3D pose estimation results"""
         load_images()
         load_results_3d()
-        core = Core(
+        core = df3d.core.Core(
             input_folder=TEST_DATA_LOCATION_WORKING,
-            output_folder="df3d",
+            output_folder=TEST_DATA_LOCATION_WORKING_RESULT,
             num_images_max=0,
             camera_ordering=[0, 1, 2, 3, 4, 5, 6],
         )
 
-        video.make_pose3d_video(core.get_points3d(), core.plot_2d, core.num_images, core.input_folder, core.output_folder)
+        df3d.video.make_pose3d_video(
+            core.get_points3d(),
+            core.plot_2d,
+            core.num_images,
+            core.input_folder,
+            core.output_folder,
+        )
 
-        video_name = 'video_pose3d_' + core.input_folder.replace('/', '_') + '.mp4'
+        video_name = "video_pose3d_" + core.input_folder.replace("/", "_") + ".mp4"
         video_path = os.path.join(core.output_folder, video_name)
-        self.assertTrue(os.path.exists(video_path), f"Video of 3D poses wasn't successfully created - does not exist at {core.output_folder}/{video_name}")
+        self.assertTrue(
+            os.path.exists(video_path),
+            f"Video of 3D poses wasn't successfully created - does not exist at {core.output_folder}/{video_name}",
+        )
 
         reference_frames = get_video_3d_frames()
         test_frames = get_video_frames(video_path)
 
-        self.assertEqual(len(test_frames), len(reference_frames), "Number of frames in output video doesn't match what it should")
-        for frame, (test_frame, reference_frame) in enumerate(zip(test_frames, reference_frames)):
-            np.testing.assert_almost_equal(test_frame, reference_frame, err_msg=f"Frame {frame} of 3D video doesn't match what it should")
+        self.assertEqual(
+            len(test_frames),
+            len(reference_frames),
+            "Number of frames in output video doesn't match what it should",
+        )
+        for frame, (test_frame, reference_frame) in enumerate(
+            zip(test_frames, reference_frames)
+        ):
+            np.testing.assert_almost_equal(
+                test_frame,
+                reference_frame,
+                err_msg=f"Frame {frame} of 3D video doesn't match what it should",
+            )
+
+    def test_cli_default_output_dir(self):
+        """
+        Test that running df3d from the cli uses the correct default output directory
+        """
+        load_videos()
+
+        subprocess.run(shlex.split("df3d-cli tests/data/working --order 0 1 2 3 4 5 6"))
+
+        reference_results = get_results_2d()
+
+        assert os.path.exists(f"{TEST_DATA_LOCATION_WORKING}_df3d"), (
+            "results folder not in default location"
+        )
+        results_file = [
+            file
+            for file in os.listdir(f"{TEST_DATA_LOCATION_WORKING}_df3d")
+            if file.startswith("df3d_result")
+        ]
+        assert len(results_file) == 1, "Couldn't find df3d_results file"
+
+        with open(f"{TEST_DATA_LOCATION_WORKING}_df3d/{results_file[0]}", "rb") as f:
+            saved_pose_data = pickle.load(f)
+
+        np.testing.assert_allclose(
+            saved_pose_data["points2d"],
+            reference_results["points2d"],
+            err_msg="2D pose estimation points not correct.",
+            atol=0.02,
+        )
+        np.testing.assert_allclose(
+            saved_pose_data["heatmap_confidence"],
+            reference_results["heatmap_confidence"],
+            err_msg="2D pose estimation confidence heatmaps not correct.",
+            atol=0.002,
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
